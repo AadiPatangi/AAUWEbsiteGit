107ff04ac1b36c1fd2197029d1f8f96919822b29
Aadi Patangi <vpatangi75@gmail.com>
2023-12-29 12:44:15 -0800

    first commit

---
+++ js.js
@@ -0,0 +1,11 @@
+// Swiper Slider 1
+var swiper = new Swiper(".mySwiper", {
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
+  // End Swiper Slider
+
+  
